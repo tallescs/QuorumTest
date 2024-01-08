888523dcd8e58e1a940db12f81835bfc5f2e6210
Talles Santana <tallescs@gmail.com>
2024-01-08 16:21:13 -0300

    Make string props as required to fix warnings

--- VoteSummary.Console/Model/Bill.cs
+++ VoteSummary.Console/Model/Bill.cs
@@ -3,7 +3,7 @@
     public class Bill
     {
         public int id { get; set; }
-        public string title { get; set; }
+        public required string title { get; set; }
         public int sponsor_id { get; set; }
     }
 }
--- VoteSummary.Console/Model/BillResult.cs
+++ VoteSummary.Console/Model/BillResult.cs
@@ -4,9 +4,9 @@ namespace VoteSummary.Console.Model
     public class BillResult
     {
         public int id {  get; set; }
-        public string title { get; set; }
+        public required string title { get; set; }
         public int supporter_count { get; set; } //count of yes votes for the bill
         public int opposer_count { get; set; } // count of no votes for the bill
-        public string primary_sponsor { get; set; } // legislator name
+        public required string primary_sponsor { get; set; } // legislator name
     }
 }
